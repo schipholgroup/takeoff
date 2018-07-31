56034ba4a1d9a527a182c1d08818ab909014edc0
Tim van Cann <timvancann@gmail.com>
2018-07-31 14:19:32 +0200

    Fix test

--- tests/test_create_databricks_secrets.py
+++ tests/test_create_databricks_secrets.py
@@ -1,4 +1,5 @@
 from pyspark_streaming_deployment import create_databricks_secrets as victim
+from pyspark_streaming_deployment.create_databricks_secrets import IdAndKey
 
 
 def test_scope_exists():
@@ -12,12 +13,17 @@ def test_scope_exists():
 
 
 def test_filter_ids():
-    ids = ['app-foo-key1', 'appfoo-key2', 'app-bar-key3', 'app-key4']
+    ids = [
+        IdAndKey('1', 'app-foo-key1'),
+        IdAndKey('2', 'appfoo-key2'),
+        IdAndKey('3', 'app-bar-key3'),
+        IdAndKey('4', 'app-key4')
+    ]
 
-    filtered = victim.__filter_ids(ids, 'app')
+    filtered = [_.key for _ in victim.__filter_ids(ids, 'app')]
     assert len(filtered) == 3
     assert all(_ in filtered for _ in ('foo-key1', 'bar-key3', 'key4'))
 
-    filtered = victim.__filter_ids(ids, 'app-foo')
+    filtered = [_.key for _ in victim.__filter_ids(ids, 'app-foo')]
     assert len(filtered) == 1
     assert 'key1' in filtered
