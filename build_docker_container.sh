627e8479eadebf1ed5682c7428f5095aad4a1d0f
Tim van Cann <timvancann@godatadriven.com>
2018-08-29 15:57:48 +0200

    Build snapshot on master

--- build_docker_container.sh
+++ build_docker_container.sh
@@ -2,18 +2,33 @@
 
 echo "Checking if commit is release tag"
 
+
 tag=`git describe --exact-match --tags HEAD`
+branch=$(git rev-parse --abbrev-ref HEAD)
+
+docker login -u ${REGISTRY_USERNAME} -p ${REGISTRY_PASSWORD} ${REGISTRY_LOGIN_SERVER}
+
+function build_and_push(){
+
+    version=$1
+    postfix=$2
+    docker_tag=${version}-${postfix}
+
+    docker_image_name=${REGISTRY_LOGIN_SERVER}/${BUILD_DEFINITIONNAME}:${docker_tag}
+
+    docker build -t ${docker_image_name} -f ./Dockerfile_${postfix} .
+    docker push ${docker_image_name}
+
+}
+
 if [[ ${tag} =~ ^([0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2})$ ]]
 then
-    docker login --username ${REGISTRY_USERNAME} --password ${REGISTRY_PASSWORD} ${REGISTRY_LOGIN_SERVER}
-
-    docker_tag=${REGISTRY_LOGIN_SERVER}/${BUILD_DEFINITIONNAME}:${tag}
-
-    docker build -t ${docker_tag}-python -f ./Dockerfile_python .
-    docker push ${docker_tag}-python
-
-    docker build -t ${docker_tag}-pyspark -f ./Dockerfile_pyspark .
-    docker push ${docker_tag}-pyspark
+    build_and_push ${tag} "python"
+    build_and_push ${tag} "pyspark"
+elif [[ "$branch" = "master" ]]
+then
+    build_and_push "SNAPSHOT" "python"
+    build_and_push "SNAPSHOT" "pyspark"
 else
-  echo "Commit is not a release, not deploying artifact"
+  echo "Commit is not a release nor master, not deploying artifact"
 fi
