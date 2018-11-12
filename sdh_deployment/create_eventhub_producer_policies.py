d7d25fc89c27f973ebbeb26462531ddb82a9dcbe
michel nossin <michel.nossin@gmail.com>
2018-11-12 09:19:59 +0100

    Remove code not needed

--- sdh_deployment/create_eventhub_producer_policies.py
+++ sdh_deployment/create_eventhub_producer_policies.py
@@ -21,30 +21,17 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
-@dataclass(frozen=True)
-class EventHubProducerPolicy(object):
-    eventhub_entity: str
-
-    @property
-    def eventhub_entity_without_environment(self):
-        """The eventhub entity is postfixed with the environment, for example: 'sdhcisseventhubdev'.
-        To have secrets in databricks environment agnostic, we remove that postfix.
-        """
-        return self.eventhub_entity[:-3]
-
-
 class CreateEventhubProducerPolicies(DeploymentStep):
     def __init__(self, env: ApplicationVersion, config: dict):
         super().__init__(env, config)
 
     def run(self):
         policies = [
-            EventHubProducerPolicy(policy["eventhubEntity"])
-            for policy in self.config["policies"]
+            policy for policy in self.config["policies"]
         ]
         self.create_eventhub_producer_policies(policies)
 
-    def create_eventhub_producer_policies(self, producer_policies: List[EventHubProducerPolicy]):
+    def create_eventhub_producer_policies(self, producer_policies: List[str]):
         logger.info(f"Using Azure resource group: {RESOURCE_GROUP}")
         logger.info(f"Using Azure namespace: {EVENTHUB_NAMESPACE}")
 
@@ -61,23 +48,23 @@ class CreateEventhubProducerPolicies(DeploymentStep):
         CreateDatabricksSecrets._create_scope(databricks_client, application_name)
 
         for policy in producer_policies:
-            eventhub_client.event_hubs.create_or_update_authorization_rule(
+            common_azure_parameters = [
                 resource_group,
                 eventhub_namespace,
-                policy.eventhub_entity + formatted_dtap,
+                policy + formatted_dtap,
                 f"{get_application_name()}-send-policy",
+            ]
+            eventhub_client.event_hubs.create_or_update_authorization_rule(
+                *common_azure_parameters,
                 [AccessRights.send],
             )
 
             connection_string = eventhub_client.event_hubs.list_keys(
-                resource_group,
-                eventhub_namespace,
-                policy.eventhub_entity + formatted_dtap,
-                f"{get_application_name()}-send-policy",
+                *common_azure_parameters
             ).primary_connection_string
 
             secret = Secret(
-                f"{policy.eventhub_entity_without_environment}-connection-string",
+                f"{policy}-connection-string",
                 connection_string)
 
             CreateDatabricksSecrets._add_secrets(databricks_client, application_name, [secret])
