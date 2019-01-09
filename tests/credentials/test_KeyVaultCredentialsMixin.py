ff02fadba2df6539f08d900663e4503bf536c91b
Tim van Cann <timvancann@godatadriven.com>
2019-01-09 13:03:16 +0100

    Add more tests

--- tests/credentials/test_KeyVaultCredentialsMixin.py
+++ tests/credentials/test_KeyVaultCredentialsMixin.py
@@ -1,5 +1,6 @@
-from pprint import pprint
 from unittest import mock
+
+from azure.keyvault.models import SecretBundle
 
 from runway.credentials.KeyVaultCredentialsMixin import KeyVaultCredentialsMixin
 
@@ -10,3 +11,19 @@ class TestKeyVaultCredentialsMixin(object):
     def test_transform_key_to_credential_kwargs(self, _):
         res = KeyVaultCredentialsMixin(None, None)._transform_key_to_credential_kwargs({'arg1': 'key1'})
         assert res == {'arg1': 'foo'}
+
+    def test_filter_keyvault_ids(self):
+        res = KeyVaultCredentialsMixin._filter_keyvault_ids(['common-username', 'common-password', 'uncommon'],
+                                                            'common')
+        assert len(res) == 2
+        assert res[0].databricks_secret_key == 'username'
+
+    @mock.patch('azure.keyvault.v7_0.key_vault_client.KeyVaultClient')
+    def test_credentials(self, client):
+        client.get_secrets.return_value = [
+        SecretBundle(id='databricks-token'),
+        SecretBundle(id='databricks-host'),
+        SecretBundle(id='some-other')]
+
+        res = KeyVaultCredentialsMixin(None, client)._credentials(['databricks-token', 'databricks-host'])
+        assert len(res) == 2
