64dc884a73c50a88d30c7d1a5c2f9b332545542a
Tim van Cann <timvancann@gmail.com>
2019-02-05 14:14:49 +0100

    Add tests

--- tests/test_deploy_to_databricks.py
+++ tests/test_deploy_to_databricks.py
@@ -1,5 +1,10 @@
 import unittest
 
+import pytest
+import voluptuous
+from mock import mock
+
+from runway.ApplicationVersion import ApplicationVersion
 from runway.deploy_to_databricks import JobConfig, DeployToDatabricks as victim
 
 jobs = [
@@ -32,45 +37,65 @@ class TestDeployToDatabricks(unittest.TestCase):
 
     def test_is_streaming_job(self):
         job_config = victim._construct_job_config(
-            config_file_fn=streaming_job_config,
-            name="app",
-            version="42",
-            egg="some.egg",
-            python_file="some.py",
+            config_file_fn=streaming_job_config
         )
         assert victim._job_is_streaming(job_config) is True
 
         job_config = victim._construct_job_config(
-            config_file_fn=batch_job_config,
-            name="app",
-            version="42",
-            egg="some.egg",
-            python_file="some.py",
+            config_file_fn=batch_job_config
         )
         assert victim._job_is_streaming(job_config) is False
 
     def test_construct_job_config(self):
         job_config = victim._construct_job_config(
             config_file_fn=streaming_job_config,
-            name="app",
-            version="42",
-            egg="some.egg",
+            application_name="app-42",
+            log_destination="app",
+            egg_file="some.egg",
             python_file="some.py",
         )
 
         assert {
-            "name": "app-42",
-            "libraries": [
-               {"egg": "some.egg"},
-               {"jar": "some.jar"}
-            ],
-            "new_cluster": {
-                "spark_version": "4.1.x-scala2.11",
-                "spark_conf": {
-                    "spark.sql.warehouse.dir": "/some_",
-                    "some.setting": "true",
-                },
-                "cluster_log_conf": {"dbfs": {"destination": "dbfs:/mnt/sdh/logs/app"}},
-            },
-            "some_int": 5,
-            "spark_python_task": {"python_file": "some.py"}} == job_config
+                   "name": "app-42",
+                   "libraries": [
+                       {"egg": "some.egg"},
+                       {"jar": "some.jar"}
+                   ],
+                   "new_cluster": {
+                       "spark_version": "4.1.x-scala2.11",
+                       "spark_conf": {
+                           "spark.sql.warehouse.dir": "/some_",
+                           "some.setting": "true",
+                       },
+                       "cluster_log_conf": {"dbfs": {"destination": "dbfs:/mnt/sdh/logs/app"}},
+                   },
+                   "some_int": 5,
+                   "spark_python_task": {"python_file": "some.py"}} == job_config
+
+    @mock.patch("runway.DeploymentStep.AzureKeyvaultClient.vault_and_client", return_value=(None, None))
+    def test_invalid_config_emtpy_schema(self, _):
+        config = {'runway_common': {'databricks_library_path': '/path'}}
+        with pytest.raises(voluptuous.error.MultipleInvalid):
+            victim(ApplicationVersion('foo', 'bar', 'baz'),
+                   config).validate()
+
+    @mock.patch("runway.DeploymentStep.AzureKeyvaultClient.vault_and_client", return_value=(None, None))
+    def test_invalid_config_emtpy_jobs(self, _):
+        config = {'runway_common': {'databricks_library_path': '/path'},
+                  'jobs': []
+                  }
+        with pytest.raises(voluptuous.error.MultipleInvalid):
+            victim(ApplicationVersion('foo', 'bar', 'baz'),
+                   config).validate()
+
+    @mock.patch("runway.DeploymentStep.AzureKeyvaultClient.vault_and_client", return_value=(None, None))
+    def test_valid_config(self, _):
+        config = {'runway_common': {'databricks_library_path': '/path'},
+                  'task': 'databricks',
+                  'jobs': [
+                      {'main_name': 'pyfile'}
+                  ]
+                  }
+        run_config = victim(ApplicationVersion('foo', 'bar', 'baz'),
+               config).validate()
+        assert run_config['jobs'][0]['lang'] == 'python'
