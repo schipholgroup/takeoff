65f2aa10744f39f0609e0552ee4cd59387fafd5d
Tim van Cann <timvancann@gmail.com>
2019-03-28 13:23:59 +0000

    Remove most Runway dependencies
    Most docker and python dependencies are now pushed
    up to the runway-base-azure image. This
    drastically decreases the build time for Runway.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM sdhcontainerregistryshared.azurecr.io/runway-base-azure:4.0.0
+FROM sdhcontainerregistryshared.azurecr.io/runway-base-azure:SNAPSHOT
 
 COPY setup.py /
 COPY README.md /
@@ -8,27 +8,5 @@ COPY runway /runway
 RUN python setup.py install
 
 RUN pip install azure-mgmt-containerservice==4.2.2 kubernetes==7.0.0
-
-# hadolint ignore=DL3008
-RUN apt-get update && apt-get install -y --no-install-recommends lsb-release apt-transport-https \
-    && apt-get clean \
-    && rm -rf /var/lib/apt/lists/*
-
-RUN apt-get update \
-    && apt-get install -y --no-install-recommends \
-    software-properties-common \
-    && curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - \
-    && apt-key fingerprint 0EBFCD88 \
-    && add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian  $(lsb_release -cs) stable" \
-    && apt-get update \
-    && apt-get install -y --no-install-recommends docker-ce
-
-# Install kubectl
-RUN curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-RUN touch /etc/apt/sources.list.d/kubernetes.list
-RUN echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
-RUN apt-get update && apt-get install -y --no-install-recommends kubectl=1.11.3-00 \
-    && apt-get clean \
-    && rm -rf /var/lib/apt/lists/*
 
 WORKDIR /src
--- setup.py
+++ setup.py
@@ -4,22 +4,18 @@ from setuptools import setup
 with open("README.md", "r") as f:
     long_description = f.read()
 
+"""
+All setup dependencies are installed in the base docker image, removing the need to reinstall the same
+dependencies every CI run.
+Feel free to add missing ones to the dependencies here. As soon as these are stable move them to
+https://github.com/Schiphol-Hub/runway-base-azure
+"""
 setup_dependencies = []
-test_dependencies = [
-    "azure==4.0.0",
-    "databricks-cli==0.8.4",
-    "jinja2==2.10",
-    "gitpython==2.1.10",
-    "mock==2.0.0",
-    "kubernetes==7.0.0",
-    "pytest==3.8.2",
-    "pytest-cov==2.6.0",
-    "py4j==0.10.7",
-    "voluptuous==0.11.5"]
-if {'pytest', 'test'}.intersection(sys.argv):
-    setup_dependencies = ['pytest-runner==4.2']
-elif {'pep8', 'flake8'}.intersection(sys.argv):
-    setup_dependencies = ['flake8==3.5.0']
+test_dependencies = []
+if {"pytest", "test"}.intersection(sys.argv):
+    setup_dependencies = ["pytest-runner==4.2"]
+elif {"pep8", "flake8"}.intersection(sys.argv):
+    setup_dependencies = ["flake8==3.5.0"]
 
 setup(
     name="Runway",
@@ -28,24 +24,7 @@ setup(
     long_description=long_description,
     author_email="SDH-Support@schiphol.nl",
     packages=["runway", "runway/credentials"],
-    install_requires=[
-        "azure==4.0.0",
-        "databricks-cli==0.8.4",
-        "docker==3.5.0",
-        "flake8==3.5.0",
-        "gitpython==2.1.10",
-        "jinja2==2.10",
-        "pytest==3.8.2",
-        "pytest-cov==2.6.0",
-        "PyYAML==3.13",
-        "requests>=2.20.0",
-        "twine==1.12.1",
-        "voluptuous==0.11.5"
-    ],
-    extras_require={
-        "test": test_dependencies,
-        "lint": ["flake8==3.5.0"],
-    },
+    install_requires=setup_dependencies,
     setup_requires=setup_dependencies,
     tests_require=test_dependencies,
     scripts=["scripts/runway", "scripts/get_version"],
