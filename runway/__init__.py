183850ee9d3cbfa8e5f754b2f86a7f2b36245336
Tim van Cann <timvancann@gmail.com>
2019-09-06 06:47:37 +0200

    Formatting

--- runway/__init__.py
+++ runway/__init__.py
@@ -4,8 +4,9 @@ root_logger = logging.getLogger()
 for handler in root_logger.handlers:
     root_logger.removeHandler(handler)
 
-logging.basicConfig(format="%(asctime)s %(levelname)s " "[%(module)s/%(funcName)s]: %(message)s",
-                    handlers=[logging.FileHandler('logs'),
-                              logging.StreamHandler()])
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s " "[%(module)s/%(funcName)s]: %(message)s",
+    handlers=[logging.FileHandler("logs"), logging.StreamHandler()],
+)
 
 logging.getLogger().setLevel(logging.INFO)
