f7aab5ea716feff6d7a01d782aa1fb335079582d
Daniel van der Ende <daniel.vanderende@gmail.com>
2019-09-09 13:11:30 +0000

    Refactor databricks secrets

--- takeoff/azure/create_databricks_secrets.py
+++ takeoff/azure/create_databricks_secrets.py
@@ -3,7 +3,6 @@ from pprint import pprint
 from typing import List
 
 import voluptuous as vol
-from databricks_cli.sdk import ApiClient
 from databricks_cli.secrets.api import SecretApi
 
 from takeoff.application_version import ApplicationVersion
@@ -27,47 +26,38 @@ class CreateDatabricksSecrets(Step):
     def __init__(self, env: ApplicationVersion, config: dict):
         super().__init__(env, config)
 
+        self.databricks_client = Databricks(self.vault_name, self.vault_client).api_client(self.config)
+        self.secret_api = SecretApi(self.databricks_client)
+
     def schema(self) -> vol.Schema:
         return SCHEMA
 
     def run(self):
         self.create_databricks_secrets()
 
-    @staticmethod
-    def _scope_exists(scopes: dict, scope_name: str):
+    def _scope_exists(self, scopes: dict, scope_name: str):
         return scope_name in set(_["name"] for _ in scopes["scopes"])
 
-    @staticmethod
-    def _create_scope(client: ApiClient, scope_name: str):
-        api = SecretApi(client)
-        scopes = api.list_scopes()
-        if not CreateDatabricksSecrets._scope_exists(scopes, scope_name):
-            api.create_scope(scope_name, None)
+    def _create_scope(self, scope_name: str):
+        scopes = self.secret_api.list_scopes()
+        if not self._scope_exists(scopes, scope_name):
+            self.secret_api.create_scope(scope_name, None)
 
-    @staticmethod
-    def _add_secrets(client: ApiClient, scope_name: str, secrets: List[Secret]):
-        api = SecretApi(client)
+    def _add_secrets(self, scope_name: str, secrets: List[Secret]):
         for secret in secrets:
             logger.info(f"Set secret {scope_name}: {secret.key}")
-            api.put_secret(scope_name, secret.key, secret.val, None)
-
-    @staticmethod
-    def _list_secrets(client, scope_name):
-        api = SecretApi(client)
-        return api.list_secrets(scope_name)
+            self.secret_api.put_secret(scope_name, secret.key, secret.val, None)
 
     def create_databricks_secrets(self):
         application_name = ApplicationName().get(self.config)
 
         secrets = self._combine_secrets(application_name)
 
-        databricks_client = Databricks(self.vault_name, self.vault_client).api_client(self.config)
-
-        self._create_scope(databricks_client, application_name)
-        self._add_secrets(databricks_client, application_name, secrets)
+        self._create_scope(application_name)
+        self._add_secrets(application_name, secrets)
 
         logging.info(f'------  {len(secrets)} secrets created in "{self.env.environment}"')
-        pprint(self._list_secrets(databricks_client, application_name))
+        pprint(self.secret_api.list_secrets(application_name))
 
     def _combine_secrets(self, application_name):
         vault_secrets = KeyVaultCredentialsMixin(self.vault_name, self.vault_client).get_keyvault_secrets(
