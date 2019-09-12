d623bf62e7b5849b5b4e2aa79e86aa7fca37e4aa
Daniel van der Ende <daniel.vanderende@gmail.com>
2019-09-12 13:34:56 +0200

    Stop printing credentials

--- takeoff/azure/credentials/service_principal.py
+++ takeoff/azure/credentials/service_principal.py
@@ -11,9 +11,7 @@ logger = logging.getLogger(__name__)
 
 class ServicePrincipalCredentials(EnvironmentCredentialsMixin):
     def credentials(self, config: dict, env: str) -> SpCredentials:
-        logging.info(config[f"ci_environment_keys_{env}"][current_filename(__file__)])
         credential_kwargs = super()._transform_environment_key_to_credential_kwargs(
             config[f"ci_environment_keys_{env}"][current_filename(__file__)]
         )
-        logging.info(credential_kwargs)
         return SpCredentials(**credential_kwargs)
