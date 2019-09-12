7d211afcff795f83fa0a5b9d9b829684ebb0887e
Daniel van der Ende <daniel.vanderende@gmail.com>
2019-09-12 13:06:50 +0200

    Debugging SP login

--- takeoff/azure/credentials/service_principal.py
+++ takeoff/azure/credentials/service_principal.py
@@ -1,12 +1,19 @@
+import logging
+
 from msrestazure.azure_active_directory import ServicePrincipalCredentials as SpCredentials
 
 from takeoff.credentials.EnvironmentCredentialsMixin import EnvironmentCredentialsMixin
 from takeoff.util import current_filename
 
 
+logger = logging.getLogger(__name__)
+
+
 class ServicePrincipalCredentials(EnvironmentCredentialsMixin):
     def credentials(self, config: dict, env: str) -> SpCredentials:
+        logging.info(config[f"ci_environment_keys_{env}"][current_filename(__file__)])
         credential_kwargs = super()._transform_environment_key_to_credential_kwargs(
             config[f"ci_environment_keys_{env}"][current_filename(__file__)]
         )
+        logging.info(credential_kwargs)
         return SpCredentials(**credential_kwargs)
